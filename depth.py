5cfccd59647a814ea1b8cbe2cdc417d5e2c63998
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-11-12 15:01:42 -0500

    some tests

---
+++ depth.py
@@ -0,0 +1,118 @@
+import numpy as np 
+import time
+import cv2
+import sys
+
+sys.setrecursionlimit(76800)
+
+def show_img (img):
+    # Changes format from float to uint8 as gradient
+    uint_img = np.array(img*255).astype('uint8')
+    grayImage = cv2.cvtColor(uint_img, cv2.COLOR_GRAY2BGR)
+    cv2.imshow("Image",grayImage)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return None
+
+#########################################
+# Import Depth Matrix 
+# datas= np.loadtxt("matrix.csv", delimiter=",", skiprows=0)
+
+# PersonaSentada
+centro_mano = (173,251)
+
+# Mano
+# centro_mano = (91,281)
+
+# Persona 2
+# centro_mano = (165,112)
+
+#Monitor
+# centro_mano = (127,137)
+
+fixed_threshold = 100
+scalar = 2
+
+def segmentate(image,bounds):
+    result = np.zeros(image.shape)
+    height, width = image.shape
+    x,y,w,h, = bounds
+    check_gradient = np.full(image.shape,False)
+    # center = (x+w/2,y+h/2)
+    center = centro_mano
+    result[centro_mano[0],centro_mano[1]] = 1
+    check_gradient[center[0],center[1]] = True
+
+    def check_pixel(pixel,gradient):
+        i_gradient = gradient[0]
+        j_gradient = gradient[1]
+        dynamic_i_min =i_gradient/scalar
+        dynamic_i_max =i_gradient*scalar
+        dynamic_j_min =j_gradient/scalar
+        dynamic_j_max =j_gradient*scalar
+        #Check right pixel
+        if pixel[0] + 1 < x+w and not check_gradient[pixel[0]+1][pixel[1]]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]+1][pixel[1]])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]+1][pixel[1]] = True
+            if dynamic_i_min < new_gradient < dynamic_i_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]+1][pixel[1]] = 1
+                check_pixel((pixel[0]+1,pixel[1]),(new_gradient,j_gradient))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]+1][pixel[1]] = 0
+
+        #Check left pixel
+        if pixel[0] - 1 > x and not check_gradient[pixel[0]-1][pixel[1]]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]-1][pixel[1]])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]-1][pixel[1]] = True
+            if dynamic_i_min < new_gradient < dynamic_i_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]-1][pixel[1]] = 1
+                check_pixel((pixel[0]-1,pixel[1]),(new_gradient,j_gradient))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]-1][pixel[1]] = 0
+
+        #Check top pixel
+        if pixel[1] - 1 > y and not check_gradient[pixel[0]][pixel[1]-1]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]][pixel[1]-1])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]][pixel[1]-1] = True
+            if dynamic_j_min < new_gradient < dynamic_j_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]][pixel[1]-1] = 1
+                check_pixel((pixel[0],pixel[1]-1),(i_gradient,new_gradient))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]][pixel[1]-1] = 0
+
+        #Check bottom pixel
+        if pixel[1] + 1 < y+h and not check_gradient[pixel[0]][pixel[1]+1]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]][pixel[1]+1])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]][pixel[1]+1] = True
+            if dynamic_j_min < new_gradient < dynamic_j_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]][pixel[1]+1] = 1
+                check_pixel((pixel[0],pixel[1]+1),(i_gradient,new_gradient))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]][pixel[1]+1] = 0
+
+    check_pixel(center,(0,0))
+    return result
+
+if __name__ == "__main__":
+    t1 = time.time()
+    img = segmentate(data, (0,0,240,320))
+    t2 = time.time()
+    print(t2-t1)
+    # show_img(data)
+    show_img(img)
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import os, platform, subprocess, re
 from numba import jit, cuda
 import numpy as np
 from timeit import default_timer as timer
+import depth
+import csv
+
+DEPTH_FOLDER = "src/depth"
+OUTPUT_FOLDER = "src/output"
+IMG_FOLDER = "src/img"
+TESTS_FOLDER = "tests"
+TEST_FILENAME = "results.csv"
+N = 10
 
 def get_processor_name():
     if platform.system() == "Windows":
@@ -26,38 +35,79 @@ def get_gpu_name():
         name = "N/A"
     return name
 
-# normal function to run on cpu
-def func(a):
-    for i in range(10000000):
-        a[i]+= 1
+# # normal function to run on cpu
+# def func(a):
+#     for i in range(10000000):
+#         a[i]+= 1
 
-# function optimized to run on gpu
-@jit(target_backend='cuda')
-def func2(a):
-    for i in range(10000000):
-        a[i]+= 1
+# # function optimized to run on gpu
+# @jit(target_backend='cuda')
+# def func2(a):
+#     for i in range(10000000):
+#         a[i]+= 1
 
-def simple_gpu_test():
-    n = 10000000
-    a = np.ones(n, dtype = np.float64)
-    start = timer()
-    func(a)
-    print("without GPU:", timer()-start)
-    start = timer()
-    func2(a)
-    print("with GPU:", timer()-start)
+# def simple_gpu_test():
+#     n = 10000000
+#     a = np.ones(n, dtype = np.float64)
+#     start = timer()
+#     func(a)
+#     print("without GPU:", timer()-start)
+#     start = timer()
+#     func2(a)
+#     print("with GPU:", timer()-start)
+
+
+def test_cpu(data):
+    img = depth.segmentate(data, (0,0,240,320))
+    return img
+
+
 
 def main():
-    proccesor = get_processor_name()
+    proccesor = get_processor_name().strip()
     gpu = get_gpu_name()
-    os = platform.system()
+    os_name = platform.system()
     os_version = platform.platform()
+    method = "our_algorithm"
     print("Processor: ", proccesor)
     print("GPU: ", gpu)
-    print("OS: ", os)
+    print("OS: ", os_name)
     print("OS version: ", os_version)
 
-    simple_gpu_test()
+    header = ["processor", "gpu", "os", "os_version", "file", "time_avg", "n", "method"]
+    # save results to csv file in the folder tests
+    if not os.path.exists(TESTS_FOLDER):
+        os.makedirs(TESTS_FOLDER)
+    if not os.path.exists(TESTS_FOLDER + "/" + TEST_FILENAME):
+        f = open(TESTS_FOLDER+"/"+TEST_FILENAME, "w", newline="")
+        writer = csv.writer(f)
+        writer.writerow(header)
+    else:
+        f = open(TESTS_FOLDER+"/"+TEST_FILENAME, "a", newline="")
+        writer = csv.writer(f)
+
+    # get all csv files in the folder DEPTH_FOLDER
+    files = os.listdir(DEPTH_FOLDER)
+    csv_files = [file for file in files if file.endswith(".csv")]
+    for file in csv_files:
+        total = 0
+        for i in range(N):
+            data = np.loadtxt(DEPTH_FOLDER+"/"+file, delimiter=",")
+            start = timer()
+            img = test_cpu(data)
+            end = timer()
+            total += end - start
+        avg = total / N
+        print("Time for file: ", file, " is: ", avg)
+        test = [proccesor, gpu, os_name, os_version, file, avg, N, method]
+        writer.writerow(test)
+        # save image to the folder OUTPUT_FOLDER
+        if not os.path.exists(OUTPUT_FOLDER):
+            os.makedirs(OUTPUT_FOLDER)
+        np.savetxt(OUTPUT_FOLDER+"/"+method+file, img, delimiter=",", fmt="%d")
+
+    f.close()
+
 
 
 if __name__ == '__main__':
