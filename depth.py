3d60a1d47b3b7fb263f9cc429c1022ec9d274a03
Abel Arismendy <abel@arismendy.co>
2022-11-15 07:43:29 -0500

    codigo doble para gpu

--- depth.py
+++ depth.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import cv2
 import sys
+from numba import jit, cuda
 
 sys.setrecursionlimit(76800)
 
@@ -38,6 +39,7 @@ centro_mano = (173,251)
 
 fixed_threshold = 100
 scalar = 2
+
 
 def segmentate(image,bounds):
     result = np.zeros(image.shape)
@@ -115,7 +117,85 @@ def segmentate(image,bounds):
     check_pixel(center,(0,0))
     return result
 
+# @jit(target_backend = 'cuda', nopython = True)
 def segmentate_iterative(image, bounds):
+    result = np.zeros(image.shape)
+    height, width = image.shape
+    x,y,w,h, = bounds
+    check_gradient = np.full(image.shape,False)
+    # center = (x+w/2,y+h/2)
+    center = centro_mano
+    result[centro_mano[0],centro_mano[1]] = 1
+    check_gradient[center[0],center[1]] = True
+    stack = [center]
+    i_gradient = 0
+    j_gradient = 0
+    dynamic_i_min = 0
+    dynamic_i_max = 0
+    dynamic_j_min = 0
+    dynamic_j_max = 0
+
+    while len(stack) > 0:
+        pixel = stack.pop()
+        #Check right pixel
+        if pixel[0] + 1 < x+w and not check_gradient[pixel[0]+1][pixel[1]]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]+1][pixel[1]])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]+1][pixel[1]] = True
+            if dynamic_i_min < new_gradient < dynamic_i_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]+1][pixel[1]] = 1
+                stack.append((pixel[0]+1,pixel[1]))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]+1][pixel[1]] = 0
+
+        #Check left pixel
+        if pixel[0] - 1 > x and not check_gradient[pixel[0]-1][pixel[1]]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]-1][pixel[1]])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]-1][pixel[1]] = True
+            if dynamic_i_min < new_gradient < dynamic_i_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]-1][pixel[1]] = 1
+                stack.append((pixel[0]-1,pixel[1]))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]-1][pixel[1]] = 0
+
+        #Check top pixel
+        if pixel[1] - 1 > y and not check_gradient[pixel[0]][pixel[1]-1]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]][pixel[1]-1])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]][pixel[1]-1] = True
+            if dynamic_j_min < new_gradient < dynamic_j_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]][pixel[1]-1] = 1
+                stack.append((pixel[0],pixel[1]-1))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]][pixel[1]-1] = 0
+
+        #Check bottom pixel
+        if pixel[1] + 1 < y+h and not check_gradient[pixel[0]][pixel[1]+1]:
+            #The pixel is inside the bounds and has not been checked
+            new_gradient = abs(image[pixel[0]][pixel[1]] - image[pixel[0]][pixel[1]+1])
+            #The gradient of the pixel in x direction was already checked
+            check_gradient[pixel[0]][pixel[1]+1] = True
+            if dynamic_j_min < new_gradient < dynamic_j_max or new_gradient < fixed_threshold:
+                #The pixel is inside the gradient and respects or is below the fixed threshold
+                result[pixel[0]][pixel[1]+1] = 1
+                stack.append((pixel[0],pixel[1]+1))
+            else:
+                #The pixel is outside the gradient
+                result[pixel[0]][pixel[1]+1] = 0
+    return result
+
+@jit(target_backend = 'cuda', nopython = True)
+def segmentate_gpu(image, bounds):
     result = np.zeros(image.shape)
     height, width = image.shape
     x,y,w,h, = bounds
